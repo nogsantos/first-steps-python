03cb69300439407a9226b0b947fb58de992a7fc5
Fabricio Nogueira <fabricio.nogueira@instruct.com.br>
2019-04-06 16:11:06 -0300

    test(test): Improve tests

--- test_guessing.py
+++ test_guessing.py
@@ -6,12 +6,18 @@ from guessing import Guessing
 class TestGuessing(unittest.TestCase):
 
     def test_if_is_bigger_when_the_number_is_bigger(self):
-        self.assertTrue(Guessing.is_biggest(11, 10))
+        guessing_game = Guessing()
+        self.assertTrue(guessing_game.is_bigger())
 
     def test_if_is_smaller_when_the_number_is_small(self):
-        self.assertFalse(Guessing.is_smaller(11, 10))
+        guessing_game = Guessing()
+        self.assertFalse(guessing_game.is_smaller())
 
     def test_if_raise_an_exception_when_no_number_are_passed(self):
         with self.assertRaises(ValueError):
-            g = Guessing()
-            g.secret()
+            guessing_game = Guessing()
+            guessing_game.secret()
+
+
+if __name__ == '__main__':
+    unittest.main()
