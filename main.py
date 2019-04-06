4fb89f96513c9319d959ed7696b9a1b4124d9457
Fabricio Nogueira <fabricio.nogueira@instruct.com.br>
2019-04-06 19:42:35 -0300

    feat(game): Create the game menu

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 # -*- coding: utf-8 -*-
 from guessing import Guessing
+import hangman
 
-Guessing()
+message = "Welcome!"
+line = "*******************************"
+header = "{}\n{}\n{}".format(line, message, line)
+print(header)
+
+print("(1) - Guessing")
+print("(2) - Hangman")
+
+game = int(input(">> Choose your game: "))
+
+if game == 1:
+    Guessing()
+elif game == 2:
+    hangman.play()
+else:
+    print("Game {} is not present".format(game))
