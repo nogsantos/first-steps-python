f04892535ab4bc609ad812efcdb4469742062bcf
Fabricio Nogueira <fabricio.nogueira@instruct.com.br>
2019-04-06 20:53:43 -0300

    feat(hangman): Add main loop

--- hangman.py
+++ hangman.py
@@ -5,6 +5,19 @@ import header
 def play():
     header.define("Welcome to the hangman game!")
 
+    secret_word = "bananaaa"
+    hanged = False
+    hit = False
+
+    while not hanged and not hit:
+
+        guess = input("Which letter?\n")
+        index = 0
+        for word in secret_word:
+            if guess.lower() == word.lower():
+                print("I found the letter {} in position {} ".format(word, index))
+            index = index + 1
+
 
 if __name__ == "__main__":
     play()
