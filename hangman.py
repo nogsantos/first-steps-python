023846c393e4b0af7ea467f919338d6f0a095806
Fabricio Nogueira <fabricio.nogueira@instruct.com.br>
2019-04-06 19:42:06 -0300

    feat(hangman): Create the hangman game

---
+++ hangman.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+
+def play():
+    message = "Welcome to the hangman game!"
+    line = "*******************************"
+    header = "{}\n{}\n{}".format(line, message, line)
+    print(header)
